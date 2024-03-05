95423099c3e769c43316fdc7db8ee924790ac0d0
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-05 14:32:35 +0100

    Add → added pseudo-code for the 1st JS Milestone

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 console.log("JS Trial")
+
+//// PASSI ESERCIZIO MILESTONE 1
+///     Recuperare come costante l'ID presente nell'HTML dell'input relativo alla distanza.
+///     Recuperare come costante l'ID presente nell'HTML dell'input relativo all'età.
+///     Recuperare come costante l'ID presente nell'HTML dell'input relativo al bottone.
+///     Testare tramite console.log di recuperare l'elemento corretto per ogni input.
+///     Definire una costante con il prezzo per km percorso.
+///     Definire una costante con il prezzo totale in base ai dati in possesso. 
+///     Utilizzare il metodo .addEventListener per recuperare le value relative ad età e distanza quando si effettua un 'click' del bottone.
+///     Creare le variabili del biglietto in base all'età seguendo 3 passaggi:
+///         1• Se l'età è minore di 18 ridurre il prezzo all' 80%.
+///         2• Se l'età è superiore a 65 ridurre il prezzo al 60%.
+///         3• Se l'età non corrisponde alle variabili su scritte, ripotrare un prezzo normale.
+///     Stampare i prezzi per ogni variabile.
+
+
